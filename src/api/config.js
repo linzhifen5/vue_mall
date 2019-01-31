7655300a9d3c77c4f2cb58b5c012887a420fd6cf
zhl <1786524543@qq.com>
2019-01-31 21:30:26 +0800

    优化数据接收，添加404图片

---
+++ src/api/config.js
@@ -0,0 +1,3 @@
+export const SUCC_CODE = 0;
+
+
--- src/api/home.js
+++ src/api/home.js
@@ -1,11 +1,27 @@
 import axios from 'axios'
+import {SUCC_CODE} from "./config";
 
 //获取幻灯片数据
 export const getHomeSlider = () => {
-  return axios.get('http://www.imooc.com/api/home/slider').then(res =>{
+  return axios.get('http://www.imooc.com/api/home/slider', {
+    timeout: 10000
+  }).then(res => {
     // console.log(res);
-    if(res.data.code === 0){
+    if (res.data.code === SUCC_CODE) {
       return res.data.slider;
     }
+
+    throw new Error('没有成功获取到数据');
+  }).catch(err => {
+    if (err) {
+      console.log(err)
+    }
+
+    return [
+      {
+        linkUrl: 'https://www.imooc.com',
+        picUrl: require('assets/img/404.png')
+      }
+    ]
   })
-}
+};
