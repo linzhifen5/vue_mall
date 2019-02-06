cb7b2f77321fa33326d3ec68452cbc15a6e3e265
zhl <1786524543@qq.com>
2019-02-06 13:16:47 +0800

    给推荐项目添加样式（首个li出现top多出1px bug）

--- src/pages/home/recommend.vue
+++ src/pages/home/recommend.vue
@@ -1,6 +1,22 @@
 <template>
-  <div>
-    HomeRecommend
+  <div class="recommend">
+    <h3 class="recommend-title">热卖推荐</h3>
+    <ul class="recommend-list">
+      <li class="recommend-item" v-for="(item,index) in recommends" :key="index">
+        <router-link class="recommend-link" :to="{name:'home-product', params:{id: item.baseinfo.itemId}}">
+          <p class="recommend-pic"><img class="recommend-img" :src="item.baseinfo.picUrl" alt=""></p>
+          <p class="recommend-name">{{item.name.shortName}}</p>
+          <p class="recommend-origPrice">
+            <del>￥{{item.price.origPrice}}</del>
+          </p>
+          <p class="recommend-info">
+            <span class="recommend-price">￥<strong class="recommend-price-num">1212</strong></span>
+            <span class="recommend-count">{{item.remind.soldCount}} 件已售 </span>
+          </p>
+          <p class="recommend-pic"><img src="" alt=""></p>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -19,14 +35,13 @@
     created() {
       this.getRecommend();
     },
-    methods:{
-      getRecommend(){
-        if(this.curPage > this.totalPage){
+    methods: {
+      getRecommend() {
+        if (this.curPage > this.totalPage) {
           return;
         }
-        getHomeRecommend(this.curPage).then(data =>{
-          if(data){
-            console.log(data);
+        getHomeRecommend(this.curPage).then(data => {
+          if (data) {
             this.curPage++;
             this.totalPage = data.totalPage;
             this.recommends = this.recommends.concat(data.itemList);
@@ -38,5 +53,63 @@
 </script>
 
 <style lang="scss" scoped>
+  @import "~assets/scss/mixins";
+
+  .recommend {
+    &-title {
+      position: relative;
+      width: 100%;
+      padding: 10px 0;
+      font-size: $font-size-l;
+      text-align: center;
+
+      &:before,
+      &:after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        width: 40%;
+        height: 1px;
+        background-color: #ddd;
+      }
+
+      &:before {
+        left: 0;
+      }
+
+      &:after {
+        right: 0;
+      }
+    }
+
+    &-list {
+      @include flex-between();
+      flex-wrap: wrap;
+    }
+
+    &-item {
+      width: 49%;
+      background-color: #fff;
+      box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.12);
+      margin-bottom: 8px;
+    }
+
+    /* 实现div内部图片位一个正方形显示技巧 */
+    &-pic { /* 图片父元素 */
+      position: relative;
+      width: 100%;
+      padding-top: 100%; /* padding-top 的高是相对于width 的。 */
+    }
+
+    &-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+
+  }
+
 
 </style>
